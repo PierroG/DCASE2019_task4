0def88ba0bc2b8041595b6f2cec918efc4a9f86a
Pierre G <pg5400@gmail.com>
2019-07-18 18:34:01 +0200

    Orange config

--- baseline/config.py
+++ baseline/config.py
@@ -13,17 +13,17 @@ eval2018 = 'dataset/metadata/validation/eval_dcase2018.csv'
 
 # config
 # prepare_data
-sample_rate = 44100
+sample_rate = 22050 # 44100
 n_window = 2048
-hop_length = 511
-n_mels = 64
+hop_length = 365 # 511
+n_mels = 128 # 64
 max_len_seconds = 10.
 max_frames = math.ceil(max_len_seconds * sample_rate / hop_length)
 
 f_min = 0.
-f_max = 22050.
+f_max = sample_rate // 2 #22050.
 
-lr = 0.0001
+lr = 0.001 #0.0001
 initial_lr = 0.
 beta1_before_rampdown = 0.9
 beta1_after_rampdown = 0.5
@@ -53,6 +53,14 @@ crnn_kwargs = {"n_in_channel": 1, "nclass": len(classes), "attention": True, "n_
                "n_layers_RNN": 2,
                 "activation": "glu",
                 "dropout": 0.5,
-               "kernel_size": 3 * [3], "padding": 3 * [1], "stride": 3 * [1], "nb_filters": [64, 64, 64],
-                "pooling": list(3 * ((2, 4),))}
-pooling_time_ratio = 8  # 2 * 2 * 2
+               "kernel_size": 7 * [3], "padding": 7 * [1], "stride": 7 * [1], "nb_filters": [16, 32, 64, 128, 128, 128, 128],
+                "pooling": list(((2,2),(2,2),(1,2),(1,2),(1,2),(1,2),(1,2)))}
+pooling_time_ratio = 4  # 2 * 2 * 2
+
+#crnn_kwargs = {"n_in_channel": 1, "nclass": len(classes), "attention": True, "n_RNN_cell": 64,
+#               "n_layers_RNN": 2,
+#                "activation": "glu",
+#                "dropout": 0.5,
+#               "kernel_size": 3 * [3], "padding": 3 * [1], "stride": 3 * [1], "nb_filters": [64, 64, 64],
+#                "pooling": list(3 * ((2, 4),))}
+#pooling_time_ratio = 8  # 2 * 2 * 2
